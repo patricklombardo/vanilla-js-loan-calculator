cf7af9e673e6e8b4ea96223d61cc90db5ece5cc1
Patrick Lombardo <LombardoP@CalSAWS.org>
2020-09-23 13:41:07 -0700

    init

---
+++ app.js
@@ -0,0 +1,54 @@
+// Listen for Submit
+
+document
+  .querySelector("#loan-form")
+  .addEventListener("submit", calculateResults);
+
+function calculateResults(e) {
+  e.preventDefault();
+
+  console.log("Calculating...");
+
+  // UI Variables
+  const amount = document.querySelector("#amount");
+  const interest = document.querySelector("#interest");
+  const years = document.querySelector("#years");
+  const monthlyPayment = document.querySelector("#monthly-payment");
+  const totalPayment = document.querySelector("#total-payment");
+  const totalInterest = document.querySelector("#total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute Monthly Payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError("Please check your numbers.");
+  }
+}
+
+// Show Error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement("div");
+
+  // Get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.append(document.createTextNode(error));
+
+  // Inset error above heading
+  card.insertBefore(errorDiv, heading);
+}
